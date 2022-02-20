dc69b95c08cbfd95b3b8b7ba4360c328188eb880
yuniskhamis <yuniskhamis3@gmail.com>
2022-02-20 00:24:32 -0500

    Initial commit

---
+++ app/src/main/java/com/yunis/businesscard/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yunis.businesscard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
